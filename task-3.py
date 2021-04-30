8124dec445582905d76cdeb74c41a32370a35c92
Anton <koldaevanton@gmail.com>
2021-04-30 15:46:02 +0300

    task 3 added

---
+++ task-3.py
@@ -0,0 +1,7 @@
+number = input("Enter number: ");
+
+single_number = int(number);
+double_number = int(number + number);
+triple_number = int(number + number + number);
+
+print(single_number + double_number + triple_number);
